ceb57dab9aa0a69a77940952ac3185b6d4cf8711
haelina <56834622+haelina@users.noreply.github.com>
2021-05-04 14:34:00 +0300

    Add function for creating openweathermap url.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
@@ -6,21 +6,31 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     lateinit var cityName: TextView
     lateinit var searchValue: EditText
 
+    fun createURL(locationName:String): URL {
+        // Insert your own API key here!
+        var key = "yourApiKey"
+        var base = "http://api.openweathermap.org/data/2.5/"
+        var args = "weather?q=$locationName&units=metric&appid=$key"
+        Log.d("MainActivity", "$base$args")
+        return URL(base + args)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cityName = findViewById(R.id.locationName)
         searchValue = findViewById(R.id.searchValue)
+        createURL("helsinki")
     }
 
     fun clickedSearch(button: View) {
         Log.d("MainActivity", "Clicked search button")
-        cityName.text = searchValue.text
+        cityName.text = searchValue.text.toString()
         searchValue.text.clear()
     }
 }
