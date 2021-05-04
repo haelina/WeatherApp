bfd4f6e3e959796953eda00a2c4c3f90670fddb8
haelina <56834622+haelina@users.noreply.github.com>
2021-05-04 13:39:22 +0300

    Place name of city to cityName and clear editText.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cityName: TextView
+    lateinit var searchValue: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cityName = findViewById(R.id.locationName)
+        searchValue = findViewById(R.id.searchValue)
     }
 
     fun clickedSearch(button: View) {
         Log.d("MainActivity", "Clicked search button")
+        cityName.text = searchValue.text
+        searchValue.text.clear()
     }
 }
