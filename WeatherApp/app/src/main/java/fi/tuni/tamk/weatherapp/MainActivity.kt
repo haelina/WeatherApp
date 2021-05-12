f129dad78d6f575c5daa0db2bed5de3b89ff1ee0
haelina <56834622+haelina@users.noreply.github.com>
2021-05-12 13:30:51 +0300

    Update values in weather data box to match results.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
@@ -12,16 +12,21 @@ import java.net.URL
 import org.apache.commons.io.IOUtils
 import java.nio.charset.StandardCharsets
 import kotlin.concurrent.thread
+import com.fasterxml.jackson.databind.ObjectMapper
 
 class MainActivity : AppCompatActivity() {
     lateinit var cityName: TextView
+    lateinit var latitude: TextView
+    lateinit var longitude: TextView
+    lateinit var temperature: TextView
     lateinit var searchValue: EditText
     lateinit var weatherData: LinearLayout
+    lateinit var currentWeather: WeatherDataObject
 
     // function for creating openweathermap url with desired location
     fun createURL(locationName:String): URL {
-        // Insert your own API key here!
-        var key = "c2ab16a9d28b8db7c07573179ae1e689"
+        // get api key from config.js
+        var key = getApiKey()
         var base = "http://api.openweathermap.org/data/2.5/"
         var args = "weather?q=$locationName&units=metric&appid=$key"
         Log.d("MainActivity", "$base$args")
@@ -31,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cityName = findViewById(R.id.locationName)
+        latitude = findViewById(R.id.latValue)
+        longitude = findViewById(R.id.lonValue)
+        temperature = findViewById(R.id.tempValue)
         searchValue = findViewById(R.id.searchValue)
         weatherData = findViewById(R.id.weatherData)
         //createURL("helsinki")
@@ -56,12 +64,26 @@ class MainActivity : AppCompatActivity() {
             inputStream.use {
                 result = IOUtils.toString(it, StandardCharsets.UTF_8)
             }
+            if(result != "") {
+                currentWeather = parseWeatherData(result)
+            }
             Log.d("MainActivity", result)
             //
             runOnUiThread() {
+                cityName.text = currentWeather.name
+                latitude.text = currentWeather.getLatitude().toString()
+                longitude.text = currentWeather.getLongitude().toString()
+                temperature.text = currentWeather.getTemperature().toString() + " \u2103"
                 weatherData.visibility = View.VISIBLE
             }
-
         }
+    }
+
+    fun parseWeatherData(data:String): WeatherDataObject {
+        Log.d("MainActivity", " data before parsing " + data)
+        val mp = ObjectMapper()
+        val dataObj:WeatherDataObject = mp.readValue(data, WeatherDataObject::class.java)
+        Log.d("MainActivity", dataObj.toString())
+        return dataObj
     }
 }
