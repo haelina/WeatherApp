8f5f7eaa74ce8569da21bd665551e8b840f8a909
haelina <56834622+haelina@users.noreply.github.com>
2021-05-17 15:24:41 +0300

    Enable search with coordinates when coordinates found.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
@@ -4,9 +4,6 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.io.IOUtils
@@ -19,7 +16,7 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
-import android.widget.Toast
+import android.widget.*
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
@@ -35,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     // latitude and longitude used on gettin weather data
     lateinit var latSearch: TextView
     lateinit var lonSearch: TextView
+    var lat = "0.0"
+    var lon = "0.0"
+    lateinit var searchWithCoordinatesButton: Button
 
     // linear layout showing weather results
     lateinit var weatherData: LinearLayout
@@ -60,6 +60,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchValue = findViewById(R.id.searchValue)
+
+        latSearch = findViewById(R.id.latitude)
+        lonSearch = findViewById(R.id.longitude)
+        searchWithCoordinatesButton = findViewById(R.id.searchWithCoordinates)
+
+        weatherData = findViewById(R.id.weatherData)
+        noResults = findViewById(R.id.notFound)
+
         cityName = findViewById(R.id.locationName)
         latitude = findViewById(R.id.latValue)
         longitude = findViewById(R.id.lonValue)
@@ -67,11 +77,7 @@ class MainActivity : AppCompatActivity() {
         temperature = findViewById(R.id.tempValue)
         minMaxTemp = findViewById(R.id.minMaxTemp)
         feelsLike = findViewById(R.id.feelsLike)
-        searchValue = findViewById(R.id.searchValue)
-        latSearch = findViewById(R.id.latitude)
-        lonSearch = findViewById(R.id.longitude)
-        weatherData = findViewById(R.id.weatherData)
-        noResults = findViewById(R.id.notFound)
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
@@ -79,16 +85,6 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         // Cancels possible location request
         cancellationTokenSource.cancel()
-    }
-
-    // function for creating openweathermap url with desired location
-    fun createURL(locationName:String): URL {
-        // get api key from config.js
-        var key = getApiKey()
-        var base = "http://api.openweathermap.org/data/2.5/"
-        var args = "weather?q=$locationName&units=metric&appid=$key"
-        Log.d("MainActivity", "$base$args")
-        return URL(base + args)
     }
 
     fun clickedSearch(button: View) {
@@ -138,22 +134,51 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun parseWeatherData(data:String): WeatherDataObject {
-        Log.d("MainActivity", " data before parsing " + data)
-        val mp = ObjectMapper()
-        val dataObj:WeatherDataObject = mp.readValue(data, WeatherDataObject::class.java)
-        Log.d("MainActivity", dataObj.toString())
-        return dataObj
+    fun clickSearchCoordinates(button: View) {
+        getWeatherWithCoordinates(lat, lon)
     }
 
-    fun clickGetCoordinates(button: View) {
-        getCoordinates()
+    fun getWeatherWithCoordinates(lat: String, lon: String) {
+        Log.d("MainActivity", "Searching for $searchValue")
+        thread() {
+            try {
+                var url: URL = createURLWithLocation(lat, lon)
+                val connection = url.openConnection() as HttpURLConnection
+                var result = ""
+                val inputStream = connection.inputStream
+
+                // 'use' automatically closes the stream in every case
+                inputStream.use {
+                    result = IOUtils.toString(it, StandardCharsets.UTF_8)
+                }
+                if(result != "") {
+                    currentWeather = parseWeatherData(result)
+                }
+                Log.d("MainActivity", result)
+
+                // update ui with runOnUiThread because we are not in main thread here
+                runOnUiThread() {
+                    cityName.text = currentWeather.name + ", " + currentWeather.getCountrycode()
+                    latitude.text = "latitude: " + currentWeather.getLatitude().toString()
+                    longitude.text = "longitude " + currentWeather.getLongitude().toString()
+                    val modifiedDescription = currentWeather.getDescription()?.substring(0, 1)?.toUpperCase() + currentWeather.getDescription()?.substring(1)
+                    description.text = modifiedDescription
+                    temperature.text = currentWeather.getTemperature().toString() + " \u2103"
+                    minMaxTemp.text = "min: " + currentWeather.getMinTemperature().toString() + " ℃ \t\t\tmax: " + currentWeather.getMaxTemperature().toString() + " ℃"
+                    feelsLike.text = "Feels like: " + currentWeather.getFeelsLikeTemperature().toString() + " ℃"
+                    weatherData.visibility = View.VISIBLE
+                }
+            } catch (e: FileNotFoundException) {
+                runOnUiThread() {
+                    noResults.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
     fun checkPermissions():Boolean {
         Log.d("MainActivity", "Checking permissions")
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // You can use the API that requires the permission.
             Log.d("MainActivity", "Permissions granted and action can be done")
             return true
         }
@@ -171,14 +196,13 @@ class MainActivity : AppCompatActivity() {
     ) {
         if(requestCode == 100) {
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //checkPermissions()
                 Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     @SuppressLint("MissingPermission")
-    fun getCoordinates() {
+    fun getCoordinates(button: View) {
         Log.d("MainActivity", "Finding coordinates")
         if(checkPermissions()) {
             if(isLocationEnabled()) {
@@ -191,6 +215,9 @@ class MainActivity : AppCompatActivity() {
                         Log.d("MainActivity", "lat:${location.latitude} lon:${location.longitude}")
                         latSearch.text = "Latitude: ${location.latitude}"
                         lonSearch.text = "Longitude: ${location.longitude}"
+                        lat = location.latitude.toString()
+                        lon = location.longitude.toString()
+                        searchWithCoordinatesButton.isEnabled = true
                     }
                 }
             } else {
