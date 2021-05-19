f8bb751bf67e76947a4ec8920513d8288ae8c25f
haelina <56834622+haelina@users.noreply.github.com>
2021-05-19 09:37:17 +0300

    Add url for gettin weather forecast data.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/OpenWeatherUrls.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/OpenWeatherUrls.kt
@@ -19,3 +19,15 @@ fun createURLWithLocation(lat:String, lon:String): URL {
     Log.d("MainActivity", "$base$args")
     return URL(base + args)
 }
+
+fun getForecastUrl(locationName: String): URL {
+    var args = "forecast?q=$locationName&units=metric&appid=$key"
+    Log.d("MainActivity", "$base$args")
+    return URL(base + args)
+}
+
+fun getForecastUrlWithCoordinates(lat:String, lon:String): URL {
+    var args = "forecast?lat=$lat&lon=$lon&units=metric&appid=$key"
+    Log.d("MainActivity", "$base$args")
+    return URL(base + args)
+}
