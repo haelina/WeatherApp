a1529f1ecf2151dd6f8c4e9bc869026f9701706c
haelina <56834622+haelina@users.noreply.github.com>
2021-05-19 17:13:06 +0300

    Rename id values.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/ForecastAdapter.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/ForecastAdapter.kt
@@ -11,13 +11,13 @@ import com.squareup.picasso.Picasso
 class ForecastAdapter(val items: ArrayList<Forecast3h>): RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
     inner class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
         var forecastIcon: ImageView
-        var nameField : TextView
-        var bmiField : TextView
+        var time : TextView
+        var temperature : TextView
 
         init {
             forecastIcon = view.findViewById(R.id.forecastIcon)
-            nameField = view.findViewById(R.id.nameText)
-            bmiField = view.findViewById(R.id.bmiText)
+            time = view.findViewById(R.id.time)
+            temperature = view.findViewById(R.id.temperature)
         }
     }
 
@@ -33,7 +33,7 @@ class ForecastAdapter(val items: ArrayList<Forecast3h>): RecyclerView.Adapter<Fo
         val forecastItem = items[position]
         Picasso.get().load("http://openweathermap.org/img/wn/${forecastItem.weather?.get(0)?.icon}@2x.png").resize(150, 150).into(holder.forecastIcon);
 
-        holder.nameField.text = "${forecastItem.dt_txt}"
-        holder.bmiField.text = "temp ${forecastItem.main?.temp} ℃"
+        holder.time.text = "${forecastItem.dt_txt}"
+        holder.temperature.text = "temp ${forecastItem.main?.temp} ℃"
     }
 }
