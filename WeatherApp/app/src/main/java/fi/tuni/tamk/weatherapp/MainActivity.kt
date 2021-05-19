658f1a6769e95912b4aaa378a7307fafb1b8c551
haelina <56834622+haelina@users.noreply.github.com>
2021-05-19 19:01:22 +0300

    Add button enabled or disabled to code.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/MainActivity.kt
@@ -34,11 +34,14 @@ class MainActivity : AppCompatActivity() {
     // city name or location name used in getting weather data
     lateinit var searchValue: EditText
 
-    // linear layout showing weather results
+    // linear layouts showing weather and forecast
     lateinit var weatherData: LinearLayout
     lateinit var forecastData: LinearLayout
     // linear layout shown when result was not found
     lateinit var noResults: LinearLayout
+
+    lateinit var weatherButton: Button
+    lateinit var forecastButton: Button
 
     // actual data fields containing weather details
     lateinit var cityName: TextView
@@ -52,6 +55,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var sunsetTime: TextView
     lateinit var weatherIcon: ImageView
     lateinit var dateTime: TextView
+
+    lateinit var forecastName: TextView
 
     // recyclerview for handling forecast items
     lateinit var recycler_view: RecyclerView
@@ -74,6 +79,9 @@ class MainActivity : AppCompatActivity() {
         forecastData = findViewById(R.id.forecastData)
         noResults = findViewById(R.id.notFound)
 
+        weatherButton = findViewById(R.id.weatherButton)
+        forecastButton = findViewById(R.id.forecastButton)
+
         cityName = findViewById(R.id.locationName)
         latitude = findViewById(R.id.latValue)
         longitude = findViewById(R.id.lonValue)
@@ -85,6 +93,8 @@ class MainActivity : AppCompatActivity() {
         sunriseTime = findViewById(R.id.sunrise)
         sunsetTime = findViewById(R.id.sunset)
         dateTime = findViewById(R.id.dateTime)
+
+        forecastName = findViewById(R.id.forecastCity)
 
         recycler_view = findViewById(R.id.recycler_view)
         recycler_view.layoutManager = LinearLayoutManager(this)
@@ -104,8 +114,12 @@ class MainActivity : AppCompatActivity() {
     fun clickedSearch(button: View) {
         noResults.visibility = View.GONE
         weatherData.visibility = View.GONE
+        forecastData.visibility = View.GONE
+        weatherButton.isEnabled = true
+        forecastButton.isEnabled = true
         Log.d("MainActivity", "Clicked search button")
         getWeather(searchValue.text.toString())
+        getForecast(searchValue.text.toString())
         cityName.text = searchValue.text.toString()
         searchValue.text.clear()
     }
@@ -144,6 +158,8 @@ class MainActivity : AppCompatActivity() {
                 }
             } catch (e: FileNotFoundException) {
                 runOnUiThread() {
+                    weatherButton.isEnabled = false
+                    forecastButton.isEnabled = false
                     noResults.visibility = View.VISIBLE
                 }
             }
@@ -151,7 +167,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateUi() {
+        weatherButton.isEnabled = true
+        forecastButton.isEnabled = true
         cityName.text = currentWeather.name + ", " + currentWeather.getCountrycode()
+        forecastName.text = currentWeather.name + ", " + currentWeather.getCountrycode()
         latitude.text = "latitude: " + currentWeather.getLatitude().toString()
         longitude.text = "longitude " + currentWeather.getLongitude().toString()
         description.text = currentWeather.getDescription()?.substring(0, 1)?.toUpperCase() + currentWeather.getDescription()?.substring(1)
@@ -173,7 +192,7 @@ class MainActivity : AppCompatActivity() {
             sunsetTime.text = "sunset: ${getTimeFormatted(sunsetDate)}"
         }
         Picasso.get().load("http://openweathermap.org/img/wn/${currentWeather.getIconCode()}@2x.png").resize(350, 350).into(weatherIcon);
-        weatherData.visibility = View.VISIBLE
+        weatherData.isVisible = true
     }
 
     fun getDateTimeFormatted(date:Date):String {
@@ -189,7 +208,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getWeatherWithCoordinates(lat: String, lon: String) {
-        Log.d("MainActivity", "Searching for $searchValue")
         thread() {
             try {
                 var url: URL = createURLWithLocation(lat, lon)
@@ -212,7 +230,9 @@ class MainActivity : AppCompatActivity() {
                 }
             } catch (e: FileNotFoundException) {
                 runOnUiThread() {
-                    noResults.visibility = View.VISIBLE
+                    weatherButton.isEnabled = false
+                    forecastButton.isEnabled = false
+                    noResults.isVisible = true
                 }
             }
         }
@@ -240,11 +260,50 @@ class MainActivity : AppCompatActivity() {
                 runOnUiThread() {
                     if(currentForecast.list != null) {
                         recycler_view.adapter = ForecastAdapter(currentForecast.list!!)
+                        weatherButton.isEnabled = true
+                        forecastButton.isEnabled = true
                     }
                 }
 
             } catch (e: FileNotFoundException) {
                 runOnUiThread() {
+                    noResults.isVisible = true
+                }
+            }
+        }
+    }
+
+    fun getForecastWithCoordinates(lat: String, lon: String) {
+        Log.d("MainActivity", "Searching for $searchValue")
+        thread() {
+            try {
+                var url: URL = getForecastUrlWithCoordinates(lat, lon)
+                val connection = url.openConnection() as HttpURLConnection
+                var result = ""
+                val inputStream = connection.inputStream
+
+                // 'use' automatically closes the stream in every case
+                inputStream.use {
+                    result = IOUtils.toString(it, StandardCharsets.UTF_8)
+                }
+                if(result != "") {
+                    currentForecast = parseForecastData(result)
+                }
+                Log.d("MainActivity", result)
+
+                // update ui with runOnUiThread because we are not in main thread here
+                runOnUiThread() {
+                    if(currentForecast.list != null) {
+                        recycler_view.adapter = ForecastAdapter(currentForecast.list!!)
+                        weatherButton.isEnabled = true
+                        forecastButton.isEnabled = true
+                    }
+                }
+            } catch (e: FileNotFoundException) {
+                runOnUiThread() {
+                    weatherButton.isEnabled = false
+                    weatherButton.alpha = 0.9f
+                    forecastButton.isEnabled = false
                     noResults.visibility = View.VISIBLE
                 }
             }
@@ -276,6 +335,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun hideLayouts() {
+        noResults.isVisible = false
+        weatherData.isVisible = false
+        forecastData.isVisible = false
+    }
+
     @SuppressLint("MissingPermission")
     fun getCoordinates(button: View) {
         Log.d("MainActivity", "Finding coordinates")
@@ -288,9 +353,9 @@ class MainActivity : AppCompatActivity() {
                         val location: Location = it.result
                         Log.d("MainActivity", "thread is ${Thread.currentThread().name}")
                         Log.d("MainActivity", "lat:${location.latitude} lon:${location.longitude}")
-                        noResults.visibility = View.GONE
-                        weatherData.visibility = View.GONE
+                        hideLayouts()
                         getWeatherWithCoordinates(location.latitude.toString(), location.longitude.toString())
+                        getForecastWithCoordinates(location.latitude.toString(), location.longitude.toString())
                     }
                 }
             } else {
