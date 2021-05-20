75ae99b07f5725b17334837d2423a9b8c59f706d
haelina <56834622+haelina@users.noreply.github.com>
2021-05-20 15:02:34 +0300

    Move date format functions to utils.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/Utils.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/Utils.kt
@@ -2,6 +2,8 @@ package fi.tuni.tamk.weatherapp
 
 import android.util.Log
 import com.fasterxml.jackson.databind.ObjectMapper
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun parseWeatherData(data:String): WeatherDataObject {
     Log.d("MainActivity", " data before parsing " + data)
@@ -17,4 +19,16 @@ fun parseForecastData(data:String): WeatherForecastObject {
     val dataObj:WeatherForecastObject = mp.readValue(data, WeatherForecastObject::class.java)
     Log.d("MainActivity", dataObj.toString())
     return dataObj
+}
+
+fun getDateTimeFormatted(date: Date):String {
+    var simpleDateFormat = SimpleDateFormat("dd.MM.yyy HH:mm:ss")
+    val formatted = simpleDateFormat.format(date)
+    return formatted
+}
+
+fun getTimeFormatted(date: Date):String {
+    var simpleDateFormat = SimpleDateFormat("HH:mm:ss")
+    val formatted = simpleDateFormat.format(date)
+    return formatted
 }
