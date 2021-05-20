6c5685c6878398911b3902a6a6ab83b7a461bfe4
haelina <56834622+haelina@users.noreply.github.com>
2021-05-20 15:01:26 +0300

    Add rain and wind values.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/ForecastAdapter.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/ForecastAdapter.kt
@@ -1,5 +1,6 @@
 package fi.tuni.tamk.weatherapp
 
+import android.text.TextUtils.substring
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,11 +13,17 @@ class ForecastAdapter(val items: ArrayList<Forecast3h>): RecyclerView.Adapter<Fo
     inner class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
         var forecastIcon: ImageView
         var time : TextView
+        var forecastDescription: TextView
+        var forecastRain: TextView
+        var forecastWind: TextView
         var temperature : TextView
 
         init {
             forecastIcon = view.findViewById(R.id.forecastIcon)
             time = view.findViewById(R.id.time)
+            forecastDescription = view.findViewById(R.id.forecastDescription)
+            forecastRain = view.findViewById(R.id.forecastRain)
+            forecastWind = view.findViewById(R.id.forecastWind)
             temperature = view.findViewById(R.id.temperature)
         }
     }
@@ -34,6 +41,10 @@ class ForecastAdapter(val items: ArrayList<Forecast3h>): RecyclerView.Adapter<Fo
         Picasso.get().load("http://openweathermap.org/img/wn/${forecastItem.weather?.get(0)?.icon}@2x.png").resize(180, 180).into(holder.forecastIcon);
 
         holder.time.text = "${forecastItem.dt_txt}"
+        holder.forecastDescription.text = "${forecastItem.weather?.get(0)?.description?.substring(0, 1)?.toUpperCase() + forecastItem.weather?.get(0)?.description?.substring(1)}"
+        if(forecastItem.rain?.threeHour == null) holder.forecastRain.text = "Rain: 0.00 mm" else holder.forecastRain.text = "Rain: ${forecastItem.rain.threeHour} mm"
+
+        holder.forecastWind.text = "Wind: ${forecastItem.wind?.speed} m/s"
         holder.temperature.text = "${forecastItem.main?.temp} ℃"
     }
 }
