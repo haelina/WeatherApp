711b399c78d45c19cc63d0aa3ed5d3c6baeb87ea
haelina <56834622+haelina@users.noreply.github.com>
2021-05-20 15:02:55 +0300

    Add rain and wind values.

--- WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/WeatherForecastObject.kt
+++ WeatherApp/app/src/main/java/fi/tuni/tamk/weatherapp/WeatherForecastObject.kt
@@ -1,29 +1,22 @@
 package fi.tuni.tamk.weatherapp
 
+import android.os.Build
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class WeatherForecastObject {
     val list:ArrayList<Forecast3h>? = null
-
-    fun getDtTxt(index:Int):String? {
-        return list?.get(index)?.dt_txt
-    }
-
-    fun getForecastTemp(index: Int):Double? {
-        return list?.get(index)?.main?.temp
-    }
-
-    fun getIconCode(index:Int):String? {
-        return list?.get(index)?.weather?.get(0)?.icon
-    }
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Forecast3h {
+    val dt:Long? = null
     val dt_txt:String? = null
     val main:MainForecast? = null
     val weather: List<ForecastDescription>? = null
+    val rain: Rain? = null
+    val wind: Wind? = null
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -35,4 +28,15 @@ class MainForecast {
 class ForecastDescription {
     val description:String? = null
     val icon:String? = null
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Rain {
+    @JsonProperty("3h")
+    val threeHour:Double? = null
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Wind {
+    val speed:Double? = null
 }
